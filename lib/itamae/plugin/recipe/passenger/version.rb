3eb4856945bd1e773521ce3e22824ee9af7ad0c4
ichylinux <ichylinux@gmail.com>
2021-05-03 13:15:59 +0900

    update passenger version to 6.0.8

--- lib/itamae/plugin/recipe/passenger/version.rb
+++ lib/itamae/plugin/recipe/passenger/version.rb
@@ -2,12 +2,12 @@ module Itamae
   module Plugin
     module Recipe
       module Passenger
-        VERSION = "0.1.1"
+        VERSION = '0.1.2'
 
         PASSENGER_VERSION = [
           PASSENGER_VERSION_MAJOR = '6',
           PASSENGER_VERSION_MINOR = '0',
-          PASSENGER_VERSION_REVISION = '6'
+          PASSENGER_VERSION_REVISION = '8'
         ].join('.')
       end
     end
