0bb942818a57038e317622effdfd92c5e1e483c1
ichylinux <ichylinux@gmail.com>
2021-11-18 17:37:36 +0900

    support passenger v6.0.12

--- lib/itamae/plugin/recipe/passenger/version.rb
+++ lib/itamae/plugin/recipe/passenger/version.rb
@@ -2,12 +2,12 @@ module Itamae
   module Plugin
     module Recipe
       module Passenger
-        VERSION = '0.1.3'
+        VERSION = '0.1.4'
 
         PASSENGER_VERSION = [
           PASSENGER_VERSION_MAJOR = '6',
           PASSENGER_VERSION_MINOR = '0',
-          PASSENGER_VERSION_REVISION = '9'
+          PASSENGER_VERSION_REVISION = '12'
         ].join('.')
       end
     end
